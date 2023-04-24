9d2c16849b0fe8b604a3e0781ffcf33a53bec7d9
RanHao-cq <79684320+RanHao-cq@users.noreply.github.com>
2023-04-24 19:54:38 +0800

    Add files via upload

---
+++ configs/flamnet/flamnet_dla34_culane.py
@@ -0,0 +1,134 @@
+net = dict(type='Detector', )
+
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+
+backbone = dict(
+    type='DLAWrapper',
+    dla='dla34',
+    pretrained=True,
+)
+
+num_points = 72
+max_lanes = 4
+sample_y = range(589, 230, -20)
+
+heads = dict(type='FLAMNetHead',
+             num_priors=192,
+             refine_layers=3,
+             fc_hidden_dim=64,
+             sample_points=36)
+
+iou_loss_weight = 2.
+cls_loss_weight = 2.
+xyt_loss_weight = 0.2
+seg_loss_weight = 1.0
+
+work_dirs = "work_dirs/flamnet/dla34_culane"
+
+neck = dict(
+        type='PPAM_DSAformer',
+        in_channels = [64,128,256,512],
+        channels=[32, 64, 128, 160],
+        out_channels=[None, 64, 64, 64],
+        decode_out_indices=[1, 2, 3],
+        depths=4,
+        num_heads=8,
+        c2t_stride=2,
+        drop_path_rate=0.2,
+        norm_cfg=norm_cfg
+    )
+
+
+test_parameters = dict(conf_threshold=0.4, nms_thres=50, nms_topk=max_lanes)
+
+epochs = 5
+batch_size = 2
+
+optimizer = dict(type='AdamW', lr=0.3e-4)  # 3e-4 for batchsize 8
+total_iter = (88880 // batch_size) * epochs
+scheduler = dict(type='CosineAnnealingLR', T_max=total_iter)
+
+eval_ep = 1
+save_ep = 1
+
+img_norm = dict(mean=[103.939, 116.779, 123.68], std=[1., 1., 1.])
+ori_img_w = 1640
+ori_img_h = 590
+img_w = 800
+img_h = 320
+cut_height = 270
+
+train_process = [
+    dict(
+        type='GenerateLaneLine',
+        transforms=[
+            dict(name='Resize',
+                 parameters=dict(size=dict(height=img_h, width=img_w)),
+                 p=1.0),
+            dict(name='HorizontalFlip', parameters=dict(p=1.0), p=0.5),
+            dict(name='ChannelShuffle', parameters=dict(p=1.0), p=0.1),
+            dict(name='MultiplyAndAddToBrightness',
+                 parameters=dict(mul=(0.85, 1.15), add=(-10, 10)),
+                 p=0.6),
+            dict(name='AddToHueAndSaturation',
+                 parameters=dict(value=(-10, 10)),
+                 p=0.7),
+            dict(name='OneOf',
+                 transforms=[
+                     dict(name='MotionBlur', parameters=dict(k=(3, 5))),
+                     dict(name='MedianBlur', parameters=dict(k=(3, 5)))
+                 ],
+                 p=0.2),
+            dict(name='Affine',
+                 parameters=dict(translate_percent=dict(x=(-0.1, 0.1),
+                                                        y=(-0.1, 0.1)),
+                                 rotate=(-10, 10),
+                                 scale=(0.8, 1.2)),
+                 p=0.7),
+            dict(name='Resize',
+                 parameters=dict(size=dict(height=img_h, width=img_w)),
+                 p=1.0),
+        ],
+    ),
+    dict(type='ToTensor', keys=['img', 'lane_line', 'seg']),
+]
+
+val_process = [
+    dict(type='GenerateLaneLine',
+         transforms=[
+             dict(name='Resize',
+                  parameters=dict(size=dict(height=img_h, width=img_w)),
+                  p=1.0),
+         ],
+         training=False),
+    dict(type='ToTensor', keys=['img']),
+]
+
+dataset_path = './data/CULane'
+dataset_type = 'CULane'
+dataset = dict(train=dict(
+    type=dataset_type,
+    data_root=dataset_path,
+    split='train',
+    processes=train_process,
+),
+val=dict(
+    type=dataset_type,
+    data_root=dataset_path,
+    split='test',
+    processes=val_process,
+),
+test=dict(
+    type=dataset_type,
+    data_root=dataset_path,
+    split='test',
+    processes=val_process,
+))
+
+workers = 12
+log_interval = 500
+# seed = 0
+num_classes = 4 + 1
+ignore_label = 255
+bg_weight = 0.4
+lr_update_by_epoch = False
